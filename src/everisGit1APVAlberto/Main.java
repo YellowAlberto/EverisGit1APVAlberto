61c54ec77f7169c7de172c3efa4c6e295b9d89d2
apiedrav <albertopiedra155@gmail.com>
2022-09-26 13:54:02 +0200

    goodbye

--- src/everisGit1APVAlberto/Main.java
+++ src/everisGit1APVAlberto/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main( String[] args )
     {
         System.out.println( "Este es un mensaje escrito por Alberto Piedra Venegas!" );
+        System.out.println( "Este es un mensaje escrito por Alberto Piedra Venegas!" );
     }
 
 }
